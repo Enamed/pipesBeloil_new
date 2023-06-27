fdaef7b4e1fb21bb59f21dad69e280d75fe4fe31
Alex <85908108+Enamed@users.noreply.github.com>
2023-06-27 20:36:24 +0300

    fronted

--- app/src/main/java/ru/netology/nmedia1022/viewmodel/ListViewModel.kt
+++ app/src/main/java/ru/netology/nmedia1022/viewmodel/ListViewModel.kt
@@ -5,21 +5,52 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+<<<<<<< HEAD
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.netology.nmedia1022.dto.Post
 import ru.netology.nmedia1022.dto.PostList
+=======
+import ru.netology.nmedia1022.dto.Post
+>>>>>>> e43862f (fronted)
 import ru.netology.nmedia1022.repository.PostRepository
 import ru.netology.nmedia1022.repository.PostRepositoryImpl
 import ru.netology.nmedia1022.utils.SingleLiveEvent
 import java.io.IOException
 import kotlin.concurrent.thread
 
+<<<<<<< HEAD
 private val empty = PostList(
     id = 0,
     naimenovanie = ""
 
 
+=======
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    likedByMe = false,
+    likes = 0,
+    published = "",
+    countShare = 0,
+    countVisio = 0,
+    naimenovanie = "",
+    diametrTrub = "127",
+    vnDimetrTrub = "51",
+    thick = "9",
+    ieu = "IEU",
+    rastiagUsilie = "12",
+    krutMoment = "12",
+    vnDavlenie = "12",
+    tipZamka = "ЗП-168",
+    narDiametrZamka = "168",
+    vnDiametrZamka = "51",
+    pin = "З-133",
+    rastiagUsilieZamka = "122",
+    krutMomentZamka = "234",
+    g105 = "S-135"
+>>>>>>> e43862f (fronted)
 )
 
 class ListViewModel(application: Application) : AndroidViewModel(application) {
@@ -37,6 +68,7 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
         loadPosts()
     }
 
+<<<<<<< HEAD
 
 
 //обновление свайпом
@@ -47,20 +79,28 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
 
 
 
+=======
+>>>>>>> e43862f (fronted)
     fun loadPosts() {
         thread {
             // Начинаем загрузку
             _data.postValue(FeedModel(loading = true))
             try {
                 // Данные успешно получены
+<<<<<<< HEAD
                 val posts = repository.getALL()
                 FeedModel(posts = posts, empty = posts.isEmpty())
+=======
+                val postslist = repository.getList()
+                FeedModel(postslist = postslist, empty = postslist.isEmpty())
+>>>>>>> e43862f (fronted)
             } catch (e: IOException) {
                 // Получена ошибка
                 FeedModel(error = true)
             }.also(_data::postValue)
         }
     }
+<<<<<<< HEAD
 
     fun refreshPosts() = viewModelScope.launch {
         thread {
@@ -121,4 +161,6 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
             }
         }
     }
+=======
+>>>>>>> e43862f (fronted)
 }
