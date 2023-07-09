3e61d769fa38639ecbb0ef2646eb483e316fefe9
Alex <85908108+Enamed@users.noreply.github.com>
2023-07-09 15:09:16 +0300

    Update for stabile working GIT

--- app/src/main/java/ru/netology/nmedia1022/viewmodel/ListViewModel.kt
+++ app/src/main/java/ru/netology/nmedia1022/viewmodel/ListViewModel.kt
@@ -5,27 +5,16 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-<<<<<<< HEAD
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 import ru.netology.nmedia1022.dto.Post
-import ru.netology.nmedia1022.dto.PostList
-=======
-import ru.netology.nmedia1022.dto.Post
->>>>>>> e43862f (fronted)
 import ru.netology.nmedia1022.repository.PostRepository
 import ru.netology.nmedia1022.repository.PostRepositoryImpl
 import ru.netology.nmedia1022.utils.SingleLiveEvent
 import java.io.IOException
 import kotlin.concurrent.thread
 
-<<<<<<< HEAD
-private val empty = PostList(
-    id = 0,
-    naimenovanie = ""
 
 
-=======
+
 private val empty = Post(
     id = 0,
     content = "",
@@ -49,8 +38,9 @@ private val empty = Post(
     pin = "З-133",
     rastiagUsilieZamka = "122",
     krutMomentZamka = "234",
-    g105 = "S-135"
->>>>>>> e43862f (fronted)
+    g105 = "S-135",
+    priznak = ""
+
 )
 
 class ListViewModel(application: Application) : AndroidViewModel(application) {
@@ -68,7 +58,6 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
         loadPosts()
     }
 
-<<<<<<< HEAD
 
 
 //обновление свайпом
@@ -79,43 +68,41 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
 
 
 
-=======
->>>>>>> e43862f (fronted)
     fun loadPosts() {
         thread {
             // Начинаем загрузку
             _data.postValue(FeedModel(loading = true))
             try {
                 // Данные успешно получены
-<<<<<<< HEAD
+
                 val posts = repository.getALL()
                 FeedModel(posts = posts, empty = posts.isEmpty())
-=======
+
                 val postslist = repository.getList()
                 FeedModel(postslist = postslist, empty = postslist.isEmpty())
->>>>>>> e43862f (fronted)
-            } catch (e: IOException) {
-                // Получена ошибка
-                FeedModel(error = true)
-            }.also(_data::postValue)
-        }
-    }
-<<<<<<< HEAD
 
-    fun refreshPosts() = viewModelScope.launch {
-        thread {
-            // Начинаем загрузку
-            _data.postValue(FeedModel(refreshing = true))
-            try {
-                // Данные успешно получены
-                val posts = repository.getALL()
-                FeedModel(posts = posts, empty = posts.isEmpty())
             } catch (e: IOException) {
                 // Получена ошибка
                 FeedModel(error = true)
             }.also(_data::postValue)
         }
     }
+
+
+//    fun refreshPosts() = viewModelScope.launch {
+//        thread {
+//            // Начинаем загрузку
+//            _data.postValue(FeedModel(refreshing = true))
+//            try {
+//                // Данные успешно получены
+//                val posts = repository.getALL()
+//                FeedModel(posts = posts, empty = posts.isEmpty())
+//            } catch (e: IOException) {
+//                // Получена ошибка
+//                FeedModel(error = true)
+//            }.also(_data::postValue)
+//        }
+//    }
 
 //    fun save() {
 //        edited.value?.let {
@@ -139,28 +126,27 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
 //        edited.value = edited.value?.copy(content = text)
 //    }
 
-    fun likeById(id: Long, likedByMe: Boolean) {
-//
-    }
+//    fun likeById(id: Long, likedByMe: Boolean) {
+////
+//    }
 
     // обновление свайпом
 
-    fun removeById(id: Long) {
-        thread {
-            // Оптимистичная модель
-            val old = _data.value?.posts.orEmpty()
-            _data.postValue(
-                _data.value?.copy(posts = _data.value?.posts.orEmpty()
-                    .filter { it.id != id }
-                )
-            )
-            try {
-                repository.removeById(id)
-            } catch (e: IOException) {
-                _data.postValue(_data.value?.copy(posts = old))
-            }
-        }
-    }
-=======
->>>>>>> e43862f (fronted)
+//    fun removeById(id: Long) {
+//        thread {
+//            // Оптимистичная модель
+//            val old = _data.value?.posts.orEmpty()
+//            _data.postValue(
+//                _data.value?.copy(posts = _data.value?.posts.orEmpty()
+//                    .filter { it.id != id }
+//                )
+//            )
+//            try {
+//                repository.removeById(id)
+//            } catch (e: IOException) {
+//                _data.postValue(_data.value?.copy(posts = old))
+//            }
+//        }
+//    }
+
 }
